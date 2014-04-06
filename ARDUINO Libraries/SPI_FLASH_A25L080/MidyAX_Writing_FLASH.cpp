e866d2f9bdb4a450650224b7c2dca1c6a7faa8a2
PALYGAP <eric.feuilleaubois@sfr.fr>
2014-04-06 19:02:31 +0200

    Added program description in files

--- ARDUINO Libraries/SPI_FLASH_A25L080/MidyAX_Writing_FLASH.cpp
+++ ARDUINO Libraries/SPI_FLASH_A25L080/MidyAX_Writing_FLASH.cpp
@@ -1,3 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// PROGRAM:     MidyAX - BCR2000 to AXE-FX MIDI communication orchestrator
+// AIM:         Provide enhanced usability of the AXE-FX by making it possible to set the
+//              AXE-FX parameters with a hardware interface (knobs/switches of the BCR2000)
+// HARDWARE:    ARDUINO MEGA 128, 4 MIDI ports with a MIDI-IN and MIDI-OUT for each port.
+// CREATOR:     Eric FEUILLEAUBOIS
+// LICENSE:     GNU license v3 - That means OPEN SOFWARE, COPYLEFT and hope it's useful to you
+// IMPORTANT Softwares/documents from other people :
+//              - ARDUINO MIDI LIBRARY by François Best
+//              - the BC MIDI Implementation.pdf by Mark van den Berg
+//              - and quite a few others
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include "Arduino.h"
 #include <SPI_FLASH_AL25L80_ELECTRONIE.h>
 
--- ARDUINO Libraries/SPI_FLASH_A25L080/SPI_FLASH_AL25L80_ELECTRONIE.h
+++ ARDUINO Libraries/SPI_FLASH_A25L080/SPI_FLASH_AL25L80_ELECTRONIE.h
@@ -1,3 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// PROGRAM:     MidyAX - BCR2000 to AXE-FX MIDI communication orchestrator
+// AIM:         Provide enhanced usability of the AXE-FX by making it possible to set the
+//              AXE-FX parameters with a hardware interface (knobs/switches of the BCR2000)
+// HARDWARE:    ARDUINO MEGA 128, 4 MIDI ports with a MIDI-IN and MIDI-OUT for each port.
+// CREATOR:     Eric FEUILLEAUBOIS
+// LICENSE:     GNU license v3 - That means OPEN SOFWARE, COPYLEFT and hope it's useful to you
+// IMPORTANT Softwares/documents from other people :
+//              - ARDUINO MIDI LIBRARY by François Best
+//              - the BC MIDI Implementation.pdf by Mark van den Berg
+//              - and quite a few others
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Defines the SPI functions of the FLASH SPI memory A25L080
 
 //  !!!!!!!!!!!!!! This files needs to be updated with the proper values !!!!!!!!!!!!!!
